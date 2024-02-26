55c45e50b1fad07bad7e161e09c3a52bcb6e17e9
KevinGirardx <89563654+KevinGirardx@users.noreply.github.com>
2024-02-26 17:51:45 -0400

    update app show

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,7 +4,7 @@
     export default {
         data() {
             return {
-                show: true,
+                show: false,
                 data: [],
                 buttons: []
             }
